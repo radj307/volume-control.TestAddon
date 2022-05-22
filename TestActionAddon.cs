833e27521e34377b659bace01e65aefc1b6ae5df
radj307 <radj307@gmail.com>
2022-05-22 03:27:43 -0400

    Initial code commit

---
+++ TestActionAddon.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using VolumeControl.API;
+using VolumeControl.Hotkeys.Attributes;
+using VolumeControl.Log;
+
+namespace VolumeControl.TestAddon
+{
+    [ActionAddon(nameof(TestActionAddon))]
+    public class TestActionAddon
+    {
+        private static VCAPI API => VCAPI.Default;
+        private static LogWriter Log => VCAPI.Log;
+
+
+        [HotkeyAction]
+        public void CustomAction(object? sender, HandledEventArgs e)
+        {
+            Log.Debug($"Successfully triggered addon method {nameof(CustomAction)}!", $"Currently selected session name: '{API.AudioAPI.SelectedSession?.ProcessName}'");
+        }
+    }
+}
